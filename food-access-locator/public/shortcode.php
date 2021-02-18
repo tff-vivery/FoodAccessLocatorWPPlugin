0f769eae434062f45538df7642895b801f6a6044
Matthew Price <mprice@webitservices.com>
2021-02-17 20:47:53 -0600

    created v1.0.6

--- food-access-locator/public/shortcode.php
+++ food-access-locator/public/shortcode.php
@@ -5,9 +5,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function food_access_locator_shortcode() {
+    $BaseUrl = get_option("food_access_locator_options")['region_token'] == "Production" ? 'https://food-access.azurewebsites.net/' : 'https://food-access-staging.azurewebsites.net/';
+
     return
-        '<iframe id="FoodAccessFrame" 
-        src="' + urlencode(get_option("food_access_locator_options")['region_token']) == "Production" ? 'https://food-access.azurewebsites.net/' : 'https://food-access-staging.azurewebsites.net/'
+        '<iframe id="FoodAccessFrame" src="' . $BaseUrl
         . '?RegionToken=' . urlencode(get_option("food_access_locator_options")['region_token']) 
         . '&DefaultRadius=' . urlencode(get_option("food_access_locator_options")["default_radius_filter"])
         . '&ShowGoogleTranslate=' . urlencode(get_option("food_access_locator_options")["show_google_translate"])
